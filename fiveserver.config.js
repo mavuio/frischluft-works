e688e6fe8952350483c3f22d15bad584423b1364
Manfred Wuits <manfred@monochrom.at>
2021-04-19 12:58:00 +0200

    initial version

---
+++ fiveserver.config.js
@@ -0,0 +1,8 @@
+// fiveserver.config.js
+module.exports = {
+    highlight: true, // enable highlight feature
+    injectBody: true, // enable instant update
+    navigate: false, // disable auto-navigation
+    remoteLogs: true | "yellow", // enable remoteLogs or choose a different color
+    browser: 'Google Chrome Canary',
+};
